90d072ce6fd98fe42327ca5fe00c724f4cc918cc
Zineb112 <zbelassiri@gmail.con>
2023-09-28 17:00:27 -0700

    2-sort.rb added

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -6,26 +6,26 @@
 
 result = []
 ARGV.each do |arg|
-    # Try to convert the argument to an integer
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
     i_arg = arg.to_i
     
-    # Check if the conversion was successful (non-zero)
-    if i_arg != 0
-        # Insert the integer argument at the right position in the sorted result
-        is_inserted = false
-        i = 0
-        l = result.size
-        while !is_inserted && i < l do
-            if result[i] < i_arg
-                i += 1
-            else
-                result.insert(i, i_arg)
-                is_inserted = true
-                break
-            end
+    # insert result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+    while !is_inserted && i < l do
+        if result[i] < i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
         end
-        result << i_arg if !is_inserted
     end
+    result << i_arg if !is_inserted
 end
 
 puts result
